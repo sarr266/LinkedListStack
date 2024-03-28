d108b245fac29652a50e4a629510d8159d0181dc
jsar26606@gmail.com <jsar26606@whatever.com>
2024-03-28 08:28:14 -0700

    just destructor for node left

--- LinkedListStack/Stack.cpp
+++ LinkedListStack/Stack.cpp
@@ -1,1 +1,43 @@
 #include "Stack.h"
+
+void Stack::Push(int val)
+{
+	Node* temp = new Node(val);
+	temp->data = val;
+	temp->next = top;
+	top = temp;
+}
+
+int Stack::Pop()
+{
+	Node* temp;
+	if (top == nullptr)
+	{
+		return -1;
+	}
+	else
+	{
+		int popped_val = top->data;
+		Node* temp = top;
+		top = top->next; 
+		delete temp; 
+		return popped_val; 
+	}
+}
+
+int Stack::Size() const
+{
+	int size = 0;
+	Node* current = top; 
+	while (current != nullptr)
+	{
+		size++; 
+		current = current->next; 
+	}
+	return size;
+}
+
+bool Stack::Empty() const
+{
+	return top == nullptr;
+}
--- LinkedListStack/Stack.h
+++ LinkedListStack/Stack.h
@@ -1,7 +1,69 @@
 #pragma once
 
+class Node
+{
+public:
+	Node(int val)
+        :
+        data(val),
+        next(nullptr)
+	{
+	}
+    Node(const Node& source)
+        :
+        data(source.data),
+        next(nullptr)
+    {
+        if (source.next != nullptr)
+        {
+            next = new Node(*source.next);
+        }
+    }
+    ~Node()
+    {
+            delete next;
+            next = nullptr;
+    }
+public:
+	int data;
+	Node* next;
+};
+
 class Stack
 {
+	Node* top;
+public:
+	Stack()
+	{
+		top = nullptr;
+	}
+
+    // Copy assignment operator
+    Stack& operator=(const Stack& source)
+    {
+        if (!Empty())
+        {
+            delete top;
+            top = nullptr;
+        }
+        if (!source.Empty())
+        {
+            top = new Node(*source.top);
+        }
+        return *this;
+    }
+
+    //Copy constructor
+    Stack(const Stack& source)
+    {
+        *this = source;
+    }
+
+    ~Stack()
+    {
+        delete top;
+        top = nullptr;
+    }
 public:
 	void Push( int val );
 	int Pop();
